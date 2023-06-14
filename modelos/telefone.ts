58ba125b5ec8e09a19cc11fcfdc63ad8c1497848
Ana marques <aclaramcpv@gmail.com>
2023-06-14 08:47:09 -0300

    atv 1 completa

--- modelos/telefone.ts
+++ modelos/telefone.ts
@@ -1,4 +1,11 @@
 export default class Telefone {
     public ddd: string
     public numero: string
+
+    public clonar(): Telefone {
+        let clone = new Telefone()
+        clone.ddd = this.ddd
+        clone.numero = this.numero
+        return clone
+    }
 }
